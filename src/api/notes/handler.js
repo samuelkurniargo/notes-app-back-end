f7d326e31d82a3e6ef7ac279e5d5f6bbe389e7cd
samuelkurniargo <samuel.kurniago@ti.ukdw.ac.id>
2021-06-25 21:06:24 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -93,6 +93,7 @@ class NotesHandler {
   async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
+      const { title, body, tags } = request.payload; 
       const { id } = request.params;
 
       await this._service.editNoteById(id, { title, body, tags });
