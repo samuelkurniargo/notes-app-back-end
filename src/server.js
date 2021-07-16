1c64cea271f7a108f2f321c2a5bc19727d88bc90
samuelkurniargo <samuel.kurniago@ti.ukdw.ac.id>
2021-07-16 20:55:14 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 // mengimport dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
 
@@ -24,6 +25,11 @@ const AuthenticationsValidator = require('./validator/authentications');
 const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
+
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
@@ -95,6 +101,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
